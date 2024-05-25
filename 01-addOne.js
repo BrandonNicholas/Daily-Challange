e6a8cc85851ed5408480f739269be3a804d827c0
BrandonNicholas <brandon.nicholas.ong@gmail.com>
2024-05-25 21:31:13 +0800

    First Commit

---
+++ 01-addOne.js
@@ -0,0 +1,14 @@
+// Challenge: 01-addOne
+// Difficulty: Basic
+// Prompt:
+// Write a function called addOne that takes a single number as an argument and returns that number plus 1.
+// Examples:
+// addOne(1) //=> 2
+// addOne(-5) //=> -4
+
+function addOne(num){
+  console.log('working'); //check if the funciton is working
+  console.log(num); // check if the call function is passing the value to the arguement
+  return num + 1;
+}
+console.log(addOne(1)) // check if the function is passing the value and returning the correct results
